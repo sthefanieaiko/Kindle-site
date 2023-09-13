a1f5c2222f912f9abc018bc088fcb8a421d97fe2
sthefanieaiko <112731883+sthefanieaiko@users.noreply.github.com>
2023-09-12 21:50:23 -0300

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -3,16 +3,18 @@ import Titulo from "@/components/Titulo";
 
 export default function Home() {
   //mock
-  const livros = [
+  const CardLivross = [
     {
+      id: 1,
       titulo: "Star Wars",
       nota: 9.5,
       poster: "https://www.themoviedb.org/t/p/w94_and_h141_bestv2/nNTvClgnd3FfQ45IVQkk4HLHuBN.jpg"
     },
     {
+      id: 2,
       titulo: "Barbie",
       nota: 9.0,
-      poster: "https://www.themoviedb.org/t/p/w94_and_h141_bestv2/yRRuLt7sMBEQkHsd1S3KaaofZn7.jpg"
+      poster: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/yRRuLt7sMBEQkHsd1S3KaaofZn7.jpg"
     }
   ] 
 
@@ -20,13 +22,13 @@ export default function Home() {
   return (
     <>
       <nav className="bg-slate-900 p-4">
-        <h1 className="text-3xl font-bold">Kindle livros</h1>
+        <h1 className="text-3xl font-bold">Kindle</h1>
       </nav>
 
       <Titulo>em alta</Titulo>
 
       <section className="flex flex-wrap gap-2">
-        {livros.map( livro => <CardLivros livro={livro} /> )}
+        {livro.map( livro => <CardFilme livro={livro} /> )}
       </section>
 
       <Titulo>lançamentos</Titulo>
