b52df9764e4bd2ba16c476b7f2265b0e1031040f
sthefanieaiko <112731883+sthefanieaiko@users.noreply.github.com>
2023-09-12 21:50:18 -0300

    Update CardLivros.jsx

--- src/Components/CardLivros.jsx
+++ src/Components/CardLivros.jsx
@@ -1,8 +1,47 @@
+"use client"
+
+import { useEffect, useState } from "react"
+import { HeartIcon } from '@heroicons/react/24/solid'
+import { HeartIcon as HeartIconOutline } from '@heroicons/react/24/outline'
 
 export default function CardLivros({livro}){
+    const [ favorito, setFavorito ] = useState(false) // hooks
+
+    useEffect(() => {
+        let favoritos = JSON.parse( localStorage.getItem("favoritos") ) || []
+        const favorito = favoritos.find(f => f.id === livro.id)
+        setFavorito(favorito)
+    }, [])
+
+    function favoritar(){
+        setFavorito(true)
+        let favoritos = JSON.parse( localStorage.getItem("favoritos") ) || []
+        favoritos.push(livro)
+        localStorage.setItem("favoritos", JSON.stringify(favoritos))
+    }
+    
+    function desfavoritar(){
+        setFavorito(false) 
+        let favoritos = JSON.parse( localStorage.getItem("favoritos") ) || []
+        const favoritosAtualizado = favoritos.filter(f => f.id !== livro.id )
+        localStorage.setItem("favoritos", JSON.stringify(favoritosAtualizado))
+    }
+
     return (
-        <div className="flex flex-col items-center gap-1 w-40 m-2">
-            <img className="rounded" src={livro.poster} alt="capa do livro" />
+        <div className="flex flex-col items-center justify-between gap-1 w-40 m-2 relative">
+            { favorito ? 
+                <HeartIcon 
+                    className="h-6 w-6 text-red-600 absolute top-1 right-2 cursor-pointer"
+                    onClick={desfavoritar}
+                 />
+                :
+                <HeartIconOutline 
+                    className="h-6 w-6 absolute top-1 right-2 cursor-pointer hover:text-red-600" 
+                    onClick={favoritar}
+                />
+            }
+
+            <img className="rounded h-56" src={livro.poster} alt="capa do livro" />
             <span className="font-bold text-lg w-full line-clamp-1 text-center">
                 {livro.titulo}
             </span>
