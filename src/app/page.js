6de139587726f92db0879cd8a60c22b22613d3e7
sthefanieaiko <112731883+sthefanieaiko@users.noreply.github.com>
2023-10-03 01:39:21 -0300

    page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
 import CardFilme from "@/components/CardFilme";
+import NavBar from "@/components/NavBar";
 import Titulo from "@/components/Titulo";
 
 async function carregarDados(){
@@ -12,12 +13,11 @@ export default async function Home() {
   
   const filmes = await carregarDados()
 
+  
+
   return (
     <>
-      <nav className="flex items-end gap-4 bg-slate-900 p-4">
-        <h1 className="text-3xl font-bold">Fiap Filmes</h1>
-        <a href="/favoritos">favoritos</a>
-      </nav>
+      <NavBar />
 
       <Titulo>em alta</Titulo>
 
